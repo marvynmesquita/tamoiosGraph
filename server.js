75d8b2fbc3dc4e81c26ab72a20527602c1e9963a
Marvyn Mesquita <31486769+marvynmesquita@users.noreply.github.com>
2024-11-22 14:14:16 -0300

    Fix: neo4j-driver não autenticando

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const neo4j = require('neo4j-driver');
 const env = require('dotenv').config();
 
 const NEO4J_URI = 'neo4j+s://684f2d7d.databases.neo4j.io'
+const user = env.parsed.NEO4J_USERNAME;
+const password = env.parsed.NEO4J_PASSWORD;
 
-const driver = neo4j.driver(NEO4J_URI, neo4j.auth.basic(env.parsed.NEO4J_USERNAME, env.parsed.NEO4J_PASSWORD));
+const driver = neo4j.driver(NEO4J_URI, neo4j.auth.basic(user, password));
 
 const init = async () => {
     
